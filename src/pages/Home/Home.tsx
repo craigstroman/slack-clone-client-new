d1a5c5a43307f8a15a36f8df89b23bd5e11d038c
Craig Stroman <cstroman@gmail.com>
2025-07-11 12:53:33 -0400

    Creating home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,5 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { Login } from '../Login/Login';
-import { Register } from '../Register/Register';
 
 export const Home: React.FC = () => {
-  return (
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>Home</div>;
 };
