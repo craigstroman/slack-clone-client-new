1eb115d24dab71a38315b4a3ddea99402e8f6930
Craig Stroman <cstroman@gmail.com>
2025-07-12 12:59:19 -0400

    Updating routes

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,14 +2,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Login } from '../Login/Login';
 import { Register } from '../Register/Register';
 import { ProtectedRoute } from '../../components/ProtectedRoutes/ProtectedRoutes';
-import { Home } from '../Home/Home';
 import { Dashboard } from '../Dashboard/Dashboard';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     children: [
-      { index: true, element: <Home /> },
+      { index: true, element: <Login /> },
       { path: 'login', element: <Login /> },
       { path: 'register', element: <Register /> },
       {
