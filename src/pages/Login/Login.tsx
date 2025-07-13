73d8d80c3574f75ae2c37ef29878c5b7872abb44
Craig Stroman <cstroman@gmail.com>
2025-07-13 16:28:30 -0400

    Updating login component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,23 +4,25 @@ import './Login.scss';
 export const Login: React.FC = () => {
   return (
     <div className="login-container">
-      <div className="form-group">
-        <input className="form-input" type="text" id="username" placeholder="Username" />
-        <label htmlFor="username" className="form-label">
-          Username:
-        </label>
-      </div>
-      <div className="form-group">
-        <input className="form-input" type="password" id="password" placeholder="Password" />
-        <label htmlFor="password" className="form-label">
-          Password:
-        </label>
-      </div>
-      <div className="form-group">
-        <button type="submit" className="form-button">
-          Login
-        </button>
-      </div>
+      <form className="login-form">
+        <div className="form-group">
+          <input className="form-input" type="text" id="username" placeholder="Username" />
+          <label htmlFor="username" className="form-label">
+            Username:
+          </label>
+        </div>
+        <div className="form-group">
+          <input className="form-input" type="password" id="password" placeholder="Password" />
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
+        </div>
+        <div className="form-group">
+          <button type="submit" className="login-button">
+            Login
+          </button>
+        </div>
+      </form>
       <div className="page-text">
         Not a registered user?&nbsp;
         <a href="/register">Sign Up</a>
