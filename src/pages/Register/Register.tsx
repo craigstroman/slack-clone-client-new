67530a6a654426c97c6ad4cc7ad6bf08a7c8565b
Craig Stroman <cstroman@gmail.com>
2025-07-13 16:28:42 -0400

    Creating register component

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,5 +1,63 @@
 import React from 'react';
+import './Register.scss';
 
 export const Register: React.FC = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register-container">
+      <form className="register-form">
+        <div className="form-group">
+          <input className="form-input" type="text" id="first_name" placeholder="First Name" />
+          <label htmlFor="first_name" className="form-label">
+            First Name:
+          </label>
+        </div>
+        <div className="form-group">
+          <input className="form-input" type="text" id="last_name" placeholder="Last Name" />
+          <label htmlFor="last_name" className="form-label">
+            Last Name:
+          </label>
+        </div>
+        <div className="form-group">
+          <input className="form-input" type="tel" id="phone_number" placeholder="Phone Number" />
+          <label htmlFor="phone_number" className="form-label">
+            Phone Number:
+          </label>
+        </div>
+        <div className="form-group">
+          <input className="form-input" type="email" id="phone_number" placeholder="Email" />
+          <label htmlFor="phone_number" className="form-label">
+            Phone Number:
+          </label>
+        </div>
+        <div className="form-group">
+          <input className="form-input" type="text" id="username" placeholder="Username" />
+          <label htmlFor="username" className="form-label">
+            Username:
+          </label>
+        </div>
+        <div className="form-group">
+          <input className="form-input" type="password" id="password" placeholder="Password" />
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
+        </div>
+        <div className="form-group">
+          <input
+            className="form-input"
+            type="password"
+            id="password_confirmation"
+            placeholder="Password Confirmation"
+          />
+          <label htmlFor="password_confirmation" className="form-label">
+            Password Confirmation:
+          </label>
+        </div>
+        <div className="form-group">
+          <button type="submit" className="register-button">
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
