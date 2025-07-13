43a2872253f8d9bf7801add8d9ef9db9f685bff3
Craig Stroman <cstroman@gmail.com>
2025-07-13 13:22:51 -0400

    Creating login component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import './Login.scss';
 
 export const Login: React.FC = () => {
-  return <div>Login page</div>;
+  return (
+    <div className="login-container">
+      <div className="form-group">
+        <input className="form-input" type="text" id="username" placeholder="Username" />
+        <label htmlFor="username" className="form-label">
+          Username:
+        </label>
+      </div>
+      <div className="form-group">
+        <input className="form-input" type="password" id="password" placeholder="Password" />
+        <label htmlFor="password" className="form-label">
+          Password:
+        </label>
+      </div>
+      <div className="form-group">
+        <button type="submit" className="form-button">
+          Login
+        </button>
+      </div>
+    </div>
+  );
 };
